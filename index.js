dd8591745857e4cc878ccc89c37114b8dad6e5b9
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-03-11 11:43:32 +0100

    Middlewares for express has been well configured

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import cors from 'cors';
 import env from 'dotenv';
 import chatRoute from './route/AIchatRoute.js'
 import { connectMongoDBatlas } from './config/dbCofig.js'
+import bodyParser from 'body-parser';
 
 env.config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 connectMongoDBatlas();
